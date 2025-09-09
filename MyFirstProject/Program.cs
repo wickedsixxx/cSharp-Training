98cf630d6ca462d30a9a9a3befb3b448ffbc57f7
Alkım Can Özaras <alkim.can@hotmail.com>
2025-09-09 16:38:19 +0300

    Add project files.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number for the FizzBuzz Game sir: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                else { Console.WriteLine(i); }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
